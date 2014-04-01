cf2f164f52b950635398cbe898ea5baab38c8d46
Kapor <smokingkapor@gmail.com>
2014-04-01 09:34:42 +0800

    Create Scrapy project

---
+++ mtchism_crawler/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for mtchism_crawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'mtchism_crawler'
+
+SPIDER_MODULES = ['mtchism_crawler.spiders']
+NEWSPIDER_MODULE = 'mtchism_crawler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'mtchism_crawler (+http://www.yourdomain.com)'
